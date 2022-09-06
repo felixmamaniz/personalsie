b44cab6f2d2fbb6755213f257a1b3de47fd060d8
Liz <lyzfernandez6@gmail.com>
2022-09-06 13:31:11 -0400

    sigue-horario

--- app/Imports/AttendancesImport.php
+++ app/Imports/AttendancesImport.php
@@ -11,42 +11,92 @@ use Maatwebsite\Excel\Concerns\WithHeadingRow;
 
 class AttendancesImport implements ToCollection, WithHeadingRow, WithBatchInserts, WithChunkReading
 {
-    public $col,$entrada,$salida;
+    public $col,$entrada,$salida,$empleado;
     /**
     * @param array $row
     *
     * @return \Illuminate\Database\Eloquent\Model|null
     */
+    //
     public function collection(Collection $rows)
     {
+        /*$unique = $rows->unique(function ($item) {
+            return $item['id_de_usuario'].$item['nombre'];
+        });
+        dump($unique);*/
         //dd($rows);
-        //separamos entra y salida en otros collection
-        $this->entrada=collect([]);
+        //separamos entra y salida en otros collection  
+        $this->empleado=collect([]);
         foreach ($rows as $row){
-            
-            if($row['estado_de_trabajo'] == "Entrada")
+            if($row['estado_de_trabajo'] == "Entrada" )
             {
-                //$this->val=dupli($rows);
-                $this->entrada->push(['id' => $row['id_de_usuario'], 'name' => $row['nombre'], 'fecha' => $row['tiempo'], 'entrada' =>substr( $row['tiempo'], 11, 9)]);
-                /*if($row['nombre']=="Crismar")
-                {dd($this->col);}*/
+                //agregamos los datos
+                $this->empleado->push(['id' => $row['id_de_usuario'], 'name' => $row['nombre'], 'fecha' => substr($row['tiempo'],0,10), 'entrada' =>substr( $row['tiempo'], 11, 9), 'salida' => null]);
                 
             }
-        }
 
+            
+            if($row['estado_de_trabajo']=="Salida")
+            {
+                $this->empleado->push(['id' => $row['id_de_usuario'], 'name' => $row['nombre'], 'fecha' => substr($row['tiempo'],0,10), 'entrada' => null, 'salida' =>substr( $row['tiempo'], 11, 9)]);
+            }
+        }
+        //dd($this->empleado);
+
+        $this->entrada=collect([]);
+        //nuevo registro donde si tengo una entrada busco si tiene una salida y si no marco no tiene salida
+        //si no tiene una entrar y solo salida la dejo como no tiene entrada
+        //si tengo una persona que tiene 2 entradas y salidas por dia guardarlas juntos
+        foreach ($this->empleado as $row){
+            
+            $sal=$this->empleado->where('fecha',$row['fecha'] and 'salida','==',null)->first();
+            dd($sal);
+
+            if($row['estado_de_trabajo'] == "Entrada" )
+            {
+                $sal=$rows->where(substr('tiempo',0,10),substr($row['tiempo'],0,10) && 'estado_de_trabajo','==','Salida')->first();
+                $sal->all();
+                dd($sal);
+                //agregamos los datos
+                $this->entrada->push(['id' => $row['id_de_usuario'], 'name' => $row['nombre'], 'fecha' => substr($row['tiempo'],0,10), 'entrada' =>substr( $row['tiempo'], 11, 9), 'salida' => 'null']);
+                
+            }
+
+            
+            if($row['estado_de_trabajo']=="Salida")
+            {
+                $this->entrada->push(['id' => $row['id_de_usuario'], 'name' => $row['nombre'], 'fecha' => substr($row['tiempo'],0,10), 'entrada' => 'null', 'salida' =>substr( $row['tiempo'], 11, 9)]);
+            }
+        }
+        dd($this->entrada);
+        //$unique = $this->entrada->unique('id');
+        $unique = $this->entrada->unique(function ($item) {
+            return $item['id'].$item['fecha'];
+        });
+         
+        $unique->values()->all(); 
+        //dump($unique);
         //dd($this->entrada);
+ 
+// [2 => 'a', 4 => 'b']
+        
         $this->salida=collect([]);
         foreach ($rows as $row){
             
             if($row['estado_de_trabajo'] == "Salida")
             {
-                $this->salida->push(['id' => $row['id_de_usuario'], 'name' => $row['nombre'], 'fecha' => $row['tiempo'], 'salida' =>substr($row['tiempo'],11,9)]);
-                /*if($row['nombre']=="Crismar")
-                {dd($this->col);}*/
+                $this->salida->push(['id' => $row['id_de_usuario'], 'name' => $row['nombre'], 'fecha' =>substr($row['tiempo'],0,10), 'salida' =>substr($row['tiempo'],11,9)]);
                 
             }
         }
-        //dd($this->salida);
+
+        $unique2 = $this->salida->unique(function ($item) {
+            return $item['id'].$item['fecha'];
+        });
+         
+        $unique2->values()->all(); 
+        dump($unique2);
+        dd($this->salida);
         //unir la entrada y salida en uno solo
         $this->empleado=collect([]);
         foreach ($this->entrada as $row){
